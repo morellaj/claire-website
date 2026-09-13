47fa4542b556a5de8948e1a4877333d81d0d8e90
Vault publish <vault@morelli.local>
2026-09-13 17:22:35 -0400

    Publish from vault: 2026-09-13

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -9,6 +9,13 @@ export interface Video {
 export const videos: Video[] = [
 
   {
+    slug: "bingsu-bead-crunch-slime",
+    title: "Bingsu bead crunch slime",
+    description: "super crunchy slime with bingsu beads",
+    youtubeId: "y9ilqnLm4C8",
+    date: "2026-09-13",
+  },
+  {
     slug: "art-time",
     title: "My introdution video",
     description: " introducing my website",
